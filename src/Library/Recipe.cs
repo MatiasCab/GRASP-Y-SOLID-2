2e9b90f6812baee1f40aa7a77f28b5260db80f39
Matias <MATIAS.CABRERA@correo.ucu.edu.uy>
2021-09-07 11:54:12 -0300

    Se agrego una una nueva clase ConsolePrinter y un nuevo metodo para calcular el total

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections;
+using System.Text;
 
 namespace Full_GRASP_And_SOLID.Library
 {
@@ -25,14 +26,29 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
-        public void PrintRecipe()
+        public double GetProductCost() // Este metodo calcula el total del producto. Se implemento sigueindo los principios SOLID  patrones GRASP
         {
-            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            double sumatoriaTotal = 0;
+            foreach (Step item in steps)
+            {
+                sumatoriaTotal+=item.StepTotal();
+            }
+            return sumatoriaTotal;
+        }
+
+        public string GetRecipeText() 
+        {
+            StringBuilder recipeText = new StringBuilder($"Receta de {this.FinalProduct.Description}:\n");
             foreach (Step step in this.steps)
             {
-                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
-                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+                recipeText.Append($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time} minutos.\n");
             }
+            recipeText.Append($"Total: {this.GetProductCost()}"); // Aca agrego el total al texto.
+            return recipeText.ToString();
         }
+        /* Aca estoy haciendo uso del patron expert, ya que como la clase recipe es la que contiene 
+        todos los datos necesarios para armar el texto a imprimir, creo un metdod que haga esa labor.
+        Porque si lo hiciera en "ConsolePrinter" estaria incumpliendo dicho patron. */
     }
 }
